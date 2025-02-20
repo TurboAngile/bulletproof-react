c20ff51d48c0d2dae24a833e9e974a851b43fd4e
guxukai <gxk@33.cn>
2025-02-20 15:20:08 +0800

    m

--- apps/react-vite/src/i18n/i18n.ts
+++ apps/react-vite/src/i18n/i18n.ts
@@ -1,30 +1,24 @@
 import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-
-// Import your translation files
-import translationEN from './en/translation.json';
-import translationFR from './fr/translation.json';
-
-// Define the resources
-const resources = {
-  en: {
-    translation: translationEN,
-  },
-  fr: {
-    translation: translationFR,
-  },
-};
+import http from 'i18next-http-backend';
+import { initReactI18next } from 'react-i18next';
 
 i18n
+  .use(http)
   .use(LanguageDetector) // Automatically detect user's language
   .use(initReactI18next) // Bind React-i18next
   .init({
-    resources,
     lng: 'en', // Default language
     fallbackLng: 'en', // Fallback language
     interpolation: {
       escapeValue: false, // React already escapes values
+      // Our interpolations are wrapped in underscores (the default
+      // is now double curly braces)
+      prefix: '__',
+      suffix: '__',
+    },
+    backend: {
+      loadPath: '/locales/__lng__.json',
     },
     debug: true, // Enable for debugging
   });
